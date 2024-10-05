00012e8bd334fe26114f98d2e16798c249bc5b54
Tejas Somani <tejassomani1134@gmail.com>
2024-10-05 16:08:25 +0530

    Tejas-Program reverse

--- TejasSomani/Reverse.java
+++ TejasSomani/Reverse.java
@@ -1,8 +1,10 @@
 package TejasSomani;
-public class Reverse{
+
+public class Reverse {
     public static void main(String[] args) {
-        for(int i =10;i>=0;i--){
-            System.out.print(i+" ");
+        for(int i = 10; i >= 0; i--) {
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 }
