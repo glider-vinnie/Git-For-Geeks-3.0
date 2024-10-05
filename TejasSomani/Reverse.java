2b4619b227cae3f2782eb316df5aaae89e7d0eb3
Tejas Somani <tejassomani1134@gmail.com>
2024-10-05 16:02:08 +0530

    tejas-reverse-printing

---
+++ TejasSomani/Reverse.java
@@ -0,0 +1,8 @@
+package TejasSomani;
+public class Reverse{
+    public static void main(String[] args) {
+        for(int i =10;i>=0;i--){
+            System.out.print(i+" ");
+        }
+    }
+}
