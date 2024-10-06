97091a1d7f0958513590addcc93cd6b61b02d2b7
Pravin nandankar <pravinnandankar03@gmail.com>
2024-10-06 12:18:39 +0530

    Added:5 c++ programs

---
+++ pravin/reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reversed = 0, remainder;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    cout << "Reversed number = " << reversed << endl;
+    return 0;
+}
